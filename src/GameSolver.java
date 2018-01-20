6090d3931a4fc312dffa4e239f15ac707ef3b513
Atid Srisukhantapuek <atid.sr@ku.th>
2018-01-20 13:08:22 +0700

    Remove TODO and Add more condition in GameSolver

--- src/GameSolver.java
+++ src/GameSolver.java
@@ -3,7 +3,7 @@ import java.util.Random;
 /**
  * Solver of guessing a secret number
  * @author Atid Srisukhantapuek
- * @version 2018.1.20
+ * @version 2018.1.18
  */
 
 public class GameSolver {
@@ -17,22 +17,22 @@ public class GameSolver {
 	public int play(NumberGame game){
 		
 		boolean correct = false;
-		int guess = 0;
+		int random = 0;
 		int upperBound = game.getUpperBound();
 		int lowerBound = 1;
 		while (!correct) {		
-			guess = upperBound + (upperBound - lowerBound)/2;
-			correct = game.guess(guess);	
-			if(game.getMessage().toLowerCase().contains("too small")) {
-				lowerBound = guess + 1;
-			}else if(game.getMessage().toLowerCase().contains("too large")) {
-				upperBound = guess - 1;
+			random = (int)(Math.random()*(upperBound - lowerBound)+lowerBound);
+			correct = game.guess(random);	
+			if(game.getMessage().toLowerCase().contains("small")) {
+				lowerBound = random + 1;
+			}else if(game.getMessage().toLowerCase().contains("large")) {
+				upperBound = random - 1;
 			}else {
 				break;
 			}
 		
 		}
 
-		return guess;
+		return random;
 	}
 }
